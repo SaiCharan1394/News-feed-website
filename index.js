0c3ddccdf486cb79108a033e206a13e953194cb5
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-07-28 15:34:35 +0530

    changes

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function getNewsFeed() {
-  console.log("get news feed was called");
-  try {
-    const data = fetch(
-      "https://api.rss2json.com/v1/api.json?rss_url=https://flipboard.com/topic/indiansports.rss"
-    );
-    console.log("getNewsFeed", data);
-    return data;
-  } catch (error) {}
-}
+async function getNewsFeed() {
+    console.log("get news feed was called");
+    try {
+      const response = await fetch(
+        "https://api.rss2json.com/v1/api.json?rss_url=https://flipboard.com/topic/indiansports.rss"
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed");
+      }
+      const data = await response.json();
+      console.log("getNewsFeed", data);
+      return data;
+    } catch (error) {
+      // Handle the error here if needed
+      console.error("Error in getNewsFeed:", error);
+      throw error; // Rethrow the error to propagate it further if desired
+    }
+  }
+  
 export { getNewsFeed };
+  
